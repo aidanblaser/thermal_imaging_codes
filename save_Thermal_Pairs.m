763202373312b6761cd901882cd27f9de5e34288
aidanblaser <ablaser@ucsd.edu>
2021-09-16 16:02:02 -0700

    Correct Longitude

--- save_Thermal_Pairs.m
+++ save_Thermal_Pairs.m
@@ -1,4 +1,4 @@
-function save_Thermal_Pairs(dirin,dirout,start,stop,undistort,metadata)
+function save_Thermal_Pairs(dirin,dirout,start,stop,undistort,metadata,geotiff)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This function extracts the data from the DJI .JPG files. For the visible
 % images, it will save the image into a matlab file. For the thermal
@@ -36,14 +36,18 @@ function save_Thermal_Pairs(dirin,dirout,start,stop,undistort,metadata)
 %                       folder Metadata/ within dirout. If you already have
 %                       the metadata you can leave this blank or 0
 %
-% cameraParams_vis, cameraParams_therm (optional)
-%  - if left blank, it will load from camera_calibration folders
-%  - can load in yourself
+% geotiff (optional) - if 1, will also save the images as geotiffs in a
+%                      folder in the outgoing directory Geotiff/. Defaults
+%                      to 0
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Written by: Aidan Blaser (ablaser@ucsd.edu)
 % Last Edited: 09/13/2021
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if nargin<7
+    geotiff = 0;
+end
 
 if nargin < 6
     metadata = 1;
@@ -62,6 +66,9 @@ end
 mkdir(dirout);
 % Create directory for metadata
 mkdir(strcat(dirin,'/Metadata'))
+if geotiff
+    mkdir(strcat(dirout,'Geotiff'))
+end
 % Load in camera parameters (for undistorting)
 if undistort
     load('camera_calibration.mat','cameraParams_vis','cameraParams_therm')
@@ -141,8 +148,8 @@ for i=start:2:stop-1
     %If there's RTK, use RTK, otherwise, use relative altitude
     try
         rtk = table_vis.RtkFlag;
-        altitude_vis = table_vis.AbsoluteAltitude + 33.69; %subtract geoid
-        altitude_therm = table_therm.AbsoluteAltitude + 33.69;
+        altitude_vis = table_vis.AbsoluteAltitude + 35.019; %subtract geoid
+        altitude_therm = table_therm.AbsoluteAltitude + 35.019;
     catch
         altitude_vis = table_vis.RelativeAltitude;
         altitude_therm = table_therm.RelativeAltitude;
@@ -150,15 +157,15 @@ for i=start:2:stop-1
     %Modify altitude for slight roll and pitch variations
     roll_vis = (table_vis.GimbalRollDegree) + table_vis.FlightRollDegree;
     pitch_vis = table_vis.GimbalPitchDegree+90 + table_vis.FlightPitchDegree;
-    yaw_vis = table_vis.FlightYawDegree + table_vis.GimbalYawDegree;
+    yaw_vis = table_vis.FlightYawDegree;
     altitude_vis_corrected = altitude_vis*sqrt(1+sqrt((sind(roll_vis)^2 + sind(pitch_vis)^2)));
     % Do the same for thermal
     roll_therm = (table_therm.GimbalRollDegree) + table_therm.FlightRollDegree;
     pitch_therm = (table_therm.GimbalPitchDegree+90) + table_therm.FlightPitchDegree;
-    yaw_therm = table_therm.FlightYawDegree + table_therm.GimbalYawDegree;
+    yaw_therm = table_therm.FlightYawDegree;
     altitude_therm_corrected = altitude_therm*sqrt(1+sqrt((sind(roll_therm)^2 + sind(pitch_therm)^2)));
     % Next, calculate the resolutions (m/pix) for each image
-    res_therm = (12.15e-3)*(altitude_therm_corrected/9);
+    res_therm = (12e-3)*(altitude_therm_corrected/9);
     res_vis = (6.17/w_vis)*(altitude_vis_corrected/4.5);
     
     % Next find Northings-Eastings coordinates for each pixel
@@ -169,8 +176,8 @@ for i=start:2:stop-1
     lat_deg_therm = table_therm.GPSLatitude;
     lon_deg_therm = table_therm.GPSLongitude;
     % Convert this to UTM
-    [vis_loc_x,vis_loc_y] = ll2utm(lat_deg_vis,-lon_deg_vis);
-    [therm_loc_x,therm_loc_y] = ll2utm(lat_deg_therm,-lon_deg_therm);
+    [vis_loc_x,vis_loc_y] = ll2utm(lat_deg_vis,lon_deg_vis);
+    [therm_loc_x,therm_loc_y] = ll2utm(lat_deg_therm,lon_deg_therm);
     
     % Using resolution, calculate utm coordinate for each pixel
     x_vis = ((0:w_vis-1)-w_vis/2)*(res_vis);
@@ -179,15 +186,17 @@ for i=start:2:stop-1
     % Rotate values and add center
     offset_x_vis = -altitude_vis_corrected*tand(roll_vis);
     offset_y_vis = -altitude_vis_corrected*(tand(pitch_vis));
-    Eastings_vis = (X_vis+offset_x_vis).*cosd(yaw_vis) + (Y_vis+offset_y_vis).*sind(yaw_vis) + vis_loc_x;
-    Northings_vis = (Y_vis+offset_y_vis).*cosd(yaw_vis) - (X_vis+offset_x_vis).*sind(yaw_vis)+ vis_loc_y;
-    % Do the same with thermal
+    Eastings_vis = (X_vis+offset_x_vis).*cosd(yaw_vis) - (Y_vis+offset_y_vis).*sind(yaw_vis) + vis_loc_x;
+    Northings_vis = -(Y_vis+offset_y_vis).*cosd(yaw_vis) - (X_vis+offset_x_vis).*sind(yaw_vis)+ vis_loc_y;
+    % Do the same with thermal 
     x_therm = ((0:w_therm-1)-w_therm/2)*(res_therm);
     y_therm = ((0:h_therm-1)-h_therm/2)*(res_therm);
     [X_therm,Y_therm] = meshgrid(x_therm,y_therm);
-    % Rotate values and add center
-    offset_x_therm = -altitude_therm_corrected*tand(roll_therm);
-    offset_y_therm = -altitude_therm_corrected*(tand(pitch_therm));
+    % Rotate values and add center (150 values determined empirically,
+    % distance in visible pixels between center of thermal and visible image
+    % respectively based on drone camera geometry)
+    offset_x_therm = -altitude_therm_corrected*tand(roll_therm) +150*res_vis;
+    offset_y_therm = -altitude_therm_corrected*(tand(pitch_therm)) - 150*res_vis;
     Eastings_therm = (X_therm+offset_x_therm).*cosd(yaw_therm) + (Y_therm+offset_y_therm).*sind(yaw_therm) + therm_loc_x;
     Northings_therm = (Y_therm+offset_y_therm).*cosd(yaw_therm) - (X_therm+offset_x_therm).*sind(yaw_therm)+ therm_loc_y;
     
@@ -238,6 +247,35 @@ for i=start:2:stop-1
     % Get folder name of dirin for naming
     slashes = strfind(dirin,'/');
     dirin_save = dirin(slashes(end-1)+1:end-1);
+    
+    
+    if geotiff
+        name2 = strcat(name,'_therm');
+        % save into geotiff
+        im_vis = imrotate(image_vis,-yaw_vis ,'loose');
+        [lat_vis_min,lon_vis_min] = utm2ll(min(Eastings_vis(:)),min(Northings_vis(:)),11);
+        [lat_vis_max,lon_vis_max] = utm2ll(max(Eastings_vis(:)),max(Northings_vis(:)),11);
+        R_vis = georasterref('LatitudeLimits',[lat_vis_min lat_vis_max],...
+            'LongitudeLimits',[lon_vis_min lon_vis_max],...
+            'RasterSize',size(im_vis),'ColumnsStartFrom','north');
+        geotiffwrite(strcat(dirout,'Geotiff/',dirin_save,'_processed_',name),im_vis,R_vis);
+        % Same for thermal
+        % First, convert thermal to color image
+        % Regrid thermal image to [0,1]
+        image_therm = image_therm-min(image_therm(:));
+        image_therm = image_therm/(max(image_therm(:)));
+        % Convert to color image
+        image_therm = uint8(255*grs2rgb(image_therm,parula));
+        % Then do same for thermal
+        im_therm = imrotate(image_therm,-yaw_therm,'loose');
+        [lat_therm_min,lon_therm_min] = utm2ll(min(Eastings_therm(:)),min(Northings_therm(:)),11);
+        [lat_therm_max,lon_therm_max] = utm2ll(max(Eastings_therm(:)),max(Northings_therm(:)),11);
+        R_therm = georasterref('LatitudeLimits',[lat_therm_min lat_therm_max],...
+            'LongitudeLimits',[lon_therm_min,lon_therm_max],...
+            'RasterSize',size(im_therm),'ColumnsStartFrom','north');
+        geotiffwrite(strcat(dirout,'Geotiff/',dirin_save,'_processed_',name2),im_therm,R_therm);
+    end
+    
     
     save(strcat(dirout,dirin_save,'_processed_',name),'DJI_data');
 
